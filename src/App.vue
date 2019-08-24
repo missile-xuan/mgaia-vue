ba62bf6cc52ddb02f8bc2f18791dcde42e682e8c
missile-xuan <missilexuan@gmail.com>
2019-08-25 00:57:50 +0800

    构建完成 使用了elementui样式

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <el-container id="app" class="background-color-base">
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    <!-- 头部 -->
+    <el-header class="header">
+      <el-menu class="el-menu-demo" :default-active="activeIndex" mode="horizontal" router=true @select="handleSelect">
+        <el-menu-item index="1" route="/">首页</el-menu-item>
+        <el-menu-item index="2" route="/about">工具</el-menu-item>
+        <el-menu-item index="3">ToDoList</el-menu-item>
+      </el-menu>
+    </el-header>
+
+    <!-- 内容 -->
+    <el-main>
+      <router-view>
+      </router-view>
+    </el-main>
+  </el-container>
+</template>
+<script>
+   export default {
+    data() {
+      return {
+        activeIndex: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
   }
-}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  height 100%
+
 </style>
