7a9c8e9e2da665a501487bed5907bd65115221c9
missile-xuan <missilexuan@gmail.com>
2019-08-30 14:26:53 +0800

    no message

---
+++ src/views/tools/components/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-input type="textarea" :autosize="{ minRows: 5, maxRows: 10}" placeholder="请输入内容" v-model="textarea">
+    </el-input>
+    <el-button type="primary" @click="getapi">获取</el-button>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/views/home/components/HelloWorld.vue'
+
+export default {
+  name: 'test',
+  components: {},
+  data () {
+    return {
+      textarea: '',
+    }
+  },
+  computed: {
+    // 计算属性的 getter
+    
+  },
+  methods:{
+    getapi: function(){
+      this.axios.get('https://www.apiopen.top/weatherApi?city=杭州').then((response) => {
+        this.textarea=JSON.stringify(response.data);
+        console.log(response.data)
+      })
+    }
+  }
+}
+
+</script>
